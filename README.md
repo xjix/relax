2a36ac281c9a9344fcd9d0dbb3039f5f222b386f
r14c <>
2022-03-25 20:34:03 +0000

    setup db package
    FossilOrigin-Name: f4a9f94162e40cd148c5b46b80879697cd31c9cef99b3479e416c1dd0691c32c

---
+++ README.md
@@ -0,0 +1,11 @@
+# relax.js
+
+information-centric networking for the web
+
+```
+mkdir relax-js
+cd relax-js
+fossil open https://source.sunshinegardens.org/xjix/relax-js
+fossil ui
+```
+
---
+++ db/src/crdt.ts
@@ -0,0 +1,79 @@
+import isArray from 'lodash/isArray'
+import isEqual from 'lodash/isEqual'
+import isNil from 'lodash/isNil'
+import isObject from 'lodash/isObject'
+import clone from '@relax/async-utils/clone'
+import mergeWith from 'lodash/mergeWith'
+import stringify from 'json-stable-stringify'
+import tail from 'lodash/tail'
+import transform from 'lodash/transform'
+
+/**
+ * replace all values in an object with `null`. used to generate the ORSet for
+ * diffing operations.
+ *
+ * @param {Object} object
+ * @return {Object}
+ */
+const nullify = (object) => transform(object, (result, value, key) => {
+  result[key] = (isObject(value) && !isArray(value))
+    ? nullify(value)
+    : null
+})
+
+/**
+ * @param {object} base
+ * @param {object} object
+ * @return {object}
+ */
+const difference = (base, object) => {
+  const changes = (object, base) => {
+    return transform(object, (result, value, key) => {
+      if (!isEqual(value, base[key])) {
+        result[key] = (isObject(value) && isObject(base[key]) && !isArray(value))
+          ? changes(value, base[key])
+          : value
+      }
+    })
+  }
+  return (isNil(base))
+    ? object
+    : changes(object, base)
+}
+
+const merge = function (object, ...sources) {
+  return mergeWith(object, ...sources, (objValue, srcValue) => {
+    if (isArray(objValue)) {
+      return srcValue
+    }
+  })
+}
+
+const observedRemoveDiff = (base, object) => {
+  const diff = difference(base, object)
+  const inverseDiff = difference(object, base)
+  const nullDiff = nullify(inverseDiff)
+  const orDiff = merge({}, nullDiff, diff)
+  return orDiff
+}
+
+/**
+ * quickly determine if two objects differ
+ *
+ * @param {Object} a
+ * @param {Object} b
+ * @returns {Boolean}
+ */
+export const diff = (a, b) => stringify(a) !== stringify(b)
+
+/**
+ * @param {Object} base
+ * @param {...Object} checkpoints
+ * @returns {Object}
+ */
+export default function () {
+  const base = arguments[0]
+  const diffs = tail(arguments).map((checkpoint) => diff(base, checkpoint))
+  const patch = merge({}, ...diffs)
+  return merge(clone(base), patch)
+}
